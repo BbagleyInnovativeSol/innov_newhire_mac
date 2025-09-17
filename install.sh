91c48890c72c4dfb1154c379cf69320be8b2f693
bbagley <bbagley@bbagley-1145.local>
2025-09-17 10:45:42 -0400

    initial commit

---
+++ install.sh
@@ -0,0 +1,34 @@
+# Install homebrew for cli and app installs
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install --cask visual-studio-code
+
+brew install --cask postman
+
+brew install --cask docker-desktop
+
+# AWS CLI
+curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+sudo installer -pkg AWSCLIV2.pkg -target /
+
+# Installs latest python version in /opt/homebrew/bin/python3.xx
+brew install python
+
+brew install node
+
+brew install mysql # Or brew install postgresql@14
+
+brew install aws-sam-cli
+
+brew install --cask session-manager-plugin
+
+brew tap xwmx/taps
+
+brew install hosts
+
+brew install sevenzip
+
+# Personal 
+brew install jq
+brew install uv #Rust built python package manager with virtualenv and dependency management
+
